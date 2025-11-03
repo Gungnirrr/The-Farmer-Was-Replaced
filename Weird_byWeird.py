0fd050aebcbf17268535559d62cbb586aa4f6d6c
Siegaii <1193727947@qq.com>
2025-11-03 23:04:20 +0800

    fix : 修复 Weird_byWeird.py 在速度等级非满级下可能产生的变量未声明问题，小草的成长时间中飞过的格子在非满级下可能飞跃奇数格子导致执行出错。推荐使用 Weird_byWeird_v2.py，无该问题并保证了最密分布

--- Weird_byWeird.py
+++ Weird_byWeird.py
@@ -1,3 +1,4 @@
+set_world_size(16)
 while True:
 	clear()
 	size = get_world_size()
@@ -14,6 +15,7 @@ while True:
 				if can_harvest():
 					harvest()
 					plant(Entities.Bush)
+					x,y = get_pos_x(),get_pos_y()
 					if x % 3 == 0 or y % 3 == 0:
 						use_item(Items.Weird_Substance)
 			move(North)
